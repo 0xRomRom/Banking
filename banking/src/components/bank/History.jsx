cda45fc10f8f51325405d5911468874b521edc24
0xRomRom <roman0811@live.nl>
2023-08-12 10:15:26 +0200

    Added filter functionality for the type

--- banking/src/components/bank/History.jsx
+++ banking/src/components/bank/History.jsx
@@ -1,6 +1,8 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUpRightAndDownLeftFromCenter } from "@fortawesome/free-solid-svg-icons";
 import stl from "./History.module.css";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const History = () => {
   const transactions = [
@@ -64,61 +66,42 @@ const History = () => {
       toFrom: "Bob Marley",
       amount: 419,
     },
-    {
-      type: "In",
-      date: "11/05/2023",
-      toFrom: "James B",
-      amount: 133.2,
-    },
-    {
-      type: "Out",
-      date: "15/06/2023",
-      toFrom: "Bob Marley",
-      amount: 419,
-    },
-    {
-      type: "In",
-      date: "11/05/2023",
-      toFrom: "James B",
-      amount: 133.2,
-    },
-    {
-      type: "Out",
-      date: "15/06/2023",
-      toFrom: "Bob Marley",
-      amount: 419,
-    },
-    {
-      type: "In",
-      date: "11/05/2023",
-      toFrom: "James B",
-      amount: 133.2,
-    },
-    {
-      type: "Out",
-      date: "15/06/2023",
-      toFrom: "Bob Marley",
-      amount: 419,
-    },
-    {
-      type: "In",
-      date: "11/05/2023",
-      toFrom: "James B",
-      amount: 133.2,
-    },
-    {
-      type: "Out",
-      date: "15/06/2023",
-      toFrom: "Bob Marley",
-      amount: 419,
-    },
   ];
+  const [filteredTransactions, setFilteredTransactions] =
+    useState(transactions);
+  const [sortOrder, setSortOrder] = useState({
+    type: "asc",
+    date: "asc",
+    toFrom: "asc",
+    amount: "asc",
+  });
+
+  // useEffect(() => {
+  //   console.log(transactions);
+  // }, [transactions]);
+
+  //Filters the type
+  const filterByType = (type) => {
+    const sortedTransactions = [...filteredTransactions].sort((a, b) => {
+      if (sortOrder[type] === "asc") {
+        return a[type].localeCompare(b[type]);
+      } else {
+        return b[type].localeCompare(a[type]);
+      }
+    });
+
+    setFilteredTransactions(sortedTransactions);
+    setSortOrder({
+      ...sortOrder,
+      [type]: sortOrder[type] === "asc" ? "desc" : "asc",
+    });
+  };
 
   return (
     <div className={stl.container}>
       <div className={stl.filterRow}>
         <ul className={stl.filterUl}>
-          <li className={stl.list}>
+          <li className={stl.list} onClick={() => filterByType("type")}>
             Type
             <FontAwesomeIcon
               icon={faUpRightAndDownLeftFromCenter}
@@ -149,7 +132,7 @@ const History = () => {
         </ul>
       </div>
       <div className={stl.listGrid}>
-        {transactions.map((item) => {
+        {filteredTransactions.map((item) => {
           return (
             <div className={stl.gridRow} key={Math.random()}>
               <span className={`${stl.rowSpan} ${stl.centered}`}>
@@ -161,6 +144,19 @@ const History = () => {
             </div>
           );
         })}
+
+        {/* {transactions.map((item) => {
+          return (
+            <div className={stl.gridRow} key={Math.random()}>
+              <span className={`${stl.rowSpan} ${stl.centered}`}>
+                {item.type}
+              </span>
+              <span className={stl.rowSpan}>{item.date}</span>
+              <span className={stl.rowSpan}>{item.toFrom}</span>
+              <span className={stl.rowSpan}>{item.amount}</span>
+            </div>
+          );
+        })} */}
       </div>
     </div>
   );
