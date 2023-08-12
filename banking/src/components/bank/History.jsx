64e746c2e52babd5ae88356b32702a4498210424
0xRomRom <roman0811@live.nl>
2023-08-12 05:56:31 +0200

    Added history grid layout

--- banking/src/components/bank/History.jsx
+++ banking/src/components/bank/History.jsx
@@ -1,7 +1,169 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUpRightAndDownLeftFromCenter } from "@fortawesome/free-solid-svg-icons";
 import stl from "./History.module.css";
 
 const History = () => {
-  return <div className={stl.container}></div>;
+  const transactions = [
+    {
+      type: "In",
+      date: "11/05/2023",
+      toFrom: "James B",
+      amount: 133.2,
+    },
+    {
+      type: "Out",
+      date: "15/06/2023",
+      toFrom: "Bob Marley",
+      amount: 419,
+    },
+    {
+      type: "In",
+      date: "11/05/2023",
+      toFrom: "James B",
+      amount: 133.2,
+    },
+    {
+      type: "Out",
+      date: "15/06/2023",
+      toFrom: "Bob Marley",
+      amount: 419,
+    },
+    {
+      type: "In",
+      date: "11/05/2023",
+      toFrom: "James B",
+      amount: 133.2,
+    },
+    {
+      type: "Out",
+      date: "15/06/2023",
+      toFrom: "Bob Marley",
+      amount: 419,
+    },
+    {
+      type: "In",
+      date: "11/05/2023",
+      toFrom: "James B",
+      amount: 133.2,
+    },
+    {
+      type: "Out",
+      date: "15/06/2023",
+      toFrom: "Bob Marley",
+      amount: 419,
+    },
+    {
+      type: "In",
+      date: "11/05/2023",
+      toFrom: "James B",
+      amount: 133.2,
+    },
+    {
+      type: "Out",
+      date: "15/06/2023",
+      toFrom: "Bob Marley",
+      amount: 419,
+    },
+    {
+      type: "In",
+      date: "11/05/2023",
+      toFrom: "James B",
+      amount: 133.2,
+    },
+    {
+      type: "Out",
+      date: "15/06/2023",
+      toFrom: "Bob Marley",
+      amount: 419,
+    },
+    {
+      type: "In",
+      date: "11/05/2023",
+      toFrom: "James B",
+      amount: 133.2,
+    },
+    {
+      type: "Out",
+      date: "15/06/2023",
+      toFrom: "Bob Marley",
+      amount: 419,
+    },
+    {
+      type: "In",
+      date: "11/05/2023",
+      toFrom: "James B",
+      amount: 133.2,
+    },
+    {
+      type: "Out",
+      date: "15/06/2023",
+      toFrom: "Bob Marley",
+      amount: 419,
+    },
+    {
+      type: "In",
+      date: "11/05/2023",
+      toFrom: "James B",
+      amount: 133.2,
+    },
+    {
+      type: "Out",
+      date: "15/06/2023",
+      toFrom: "Bob Marley",
+      amount: 419,
+    },
+  ];
+
+  return (
+    <div className={stl.container}>
+      <div className={stl.filterRow}>
+        <ul className={stl.filterUl}>
+          <li className={stl.list}>
+            Type
+            <FontAwesomeIcon
+              icon={faUpRightAndDownLeftFromCenter}
+              className={stl.arrows}
+            />
+          </li>
+          <li className={stl.list}>
+            Date
+            <FontAwesomeIcon
+              icon={faUpRightAndDownLeftFromCenter}
+              className={stl.arrows}
+            />
+          </li>
+          <li className={stl.list}>
+            To/From
+            <FontAwesomeIcon
+              icon={faUpRightAndDownLeftFromCenter}
+              className={stl.arrows}
+            />
+          </li>
+          <li className={stl.list}>
+            Amount
+            <FontAwesomeIcon
+              icon={faUpRightAndDownLeftFromCenter}
+              className={stl.arrows}
+            />
+          </li>
+        </ul>
+      </div>
+      <div className={stl.listGrid}>
+        {transactions.map((item) => {
+          return (
+            <div className={stl.gridRow} key={Math.random()}>
+              <span className={`${stl.rowSpan} ${stl.centered}`}>
+                {item.type}
+              </span>
+              <span className={stl.rowSpan}>{item.date}</span>
+              <span className={stl.rowSpan}>{item.toFrom}</span>
+              <span className={stl.rowSpan}>{item.amount}</span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default History;
